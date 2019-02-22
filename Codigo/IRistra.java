969467b8bd7746ff09590d78efe3e1f34ccf73b2
Usuario <Usuario@DESKTOP-P9PU196>
2019-02-22 22:17:18 +0100

    commit buen funcionamiento y puntua BIEN

--- Codigo/IRistra.java
+++ Codigo/IRistra.java
@@ -6,12 +6,11 @@ import java.util.ArrayList;
 
 public interface IRistra {
    public abstract void nuevaBola(Bola bola);
-   public abstract int longitud();
-   public abstract void mostrarAñadiendoDerecha();
-   public abstract ArrayList<Bola> invertir();
    public abstract void mostrarConFormato(ArrayList<Bola> bola);
    public abstract ArrayList<Bola> disparar(Bola bola)throws FueraDeRango,ListaVacia ;
    public abstract void explotar(int posicion, int acumulador, int acumulador2) throws ListaVacia;
+    public abstract void concatenarExplosiones(Bola bola, int control) throws FueraDeRango, ListaVacia;
    public abstract int puntuacion(int bolasJuntas);
-   public abstract void concatenarExplosiones(Bola bola, int control) throws FueraDeRango, ListaVacia;
+   public abstract int puntuacionConcatenacion();
+  
 }
