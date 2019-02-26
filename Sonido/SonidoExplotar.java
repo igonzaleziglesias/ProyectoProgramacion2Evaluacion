b4361cf12e9a290ffc9094dfb6e63001d99556f9
Usuario <Usuario@DESKTOP-P9PU196>
2019-02-26 23:12:07 +0100

    commit con buen funcionamiento, puntuacion,niveles y huevo de pascua y sonido

--- Sonido/SonidoExplotar.java
+++ Sonido/SonidoExplotar.java
@@ -9,7 +9,8 @@ public class SonidoExplotar {
     public SonidoExplotar(){
         try {
             Clip sonido = AudioSystem.getClip();
-            File a = new File("/home/local/DANIELCASTELAO/igonzaleziglesias/Escritorio/Balloon.wav");
+            File a = new File("Balloon.wav.wav");
+//            File a = new File("/home/local/DANIELCASTELAO/igonzaleziglesias/Escritorio/Balloon.wav");
             sonido.open(AudioSystem.getAudioInputStream(a));
             sonido.start();
 //            System.out.println("Reproduciendo 0.2s. de sonido...");
