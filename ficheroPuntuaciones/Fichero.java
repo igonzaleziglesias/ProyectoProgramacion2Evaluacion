e5cd9d09a071859cf2ac0d2f66742cb276856992
Usuario <Usuario@DESKTOP-P9PU196>
2019-03-07 17:09:06 +0100

    zuma FALTA METER PROBANDOFICHEROS

--- ficheroPuntuaciones/Fichero.java
+++ ficheroPuntuaciones/Fichero.java
@@ -14,21 +14,21 @@ public class Fichero {
     private PrintWriter f = null;
     Scanner sc;
 
-    public Fichero() {
+    public Fichero() {//creamos el fichero
 //        fich = new File("/home/local/DANIELCASTELAO/igonzaleziglesias/Escritorio/puntuaciones.txt");
         fich = new File("C:\\Users\\Usuario\\Desktop\\puntuaciones.txt");
     }
 
-    public void añadirPuntuacion(int puntuacion) {
+    public void añadirPuntuacion(int puntuacion) {//añadimos una puntuacion de un usuario
         try {
 //            f = new PrintWriter(new FileWriter("/home/local/DANIELCASTELAO/igonzaleziglesias/Escritorio/puntuaciones.txt", true));
-            f = new PrintWriter(new FileWriter("C:\\Users\\Usuario\\Desktop\\puntuaciones.txt", true));
-            Usuario jugador = new Usuario(Pedir.pedirString("Nick Name: "), puntuacion);
-            f.println(jugador);
+            f = new PrintWriter(new FileWriter("C:\\Users\\Usuario\\Desktop\\puntuaciones.txt", true));//abrimos canal
+            Usuario jugador = new Usuario(Pedir.pedirString("Nick Name: "), puntuacion);//pedimos el nick y asignamos la puntuacion
+            f.println(jugador);//escribimos en el fichero
         } catch (IOException ex) {
             System.out.println("error1 " + ex.getMessage());
         } finally {
-            f.close();
+            f.close();//cerramos canal
         }
     }
 
@@ -37,12 +37,12 @@ public class Fichero {
             sc = new Scanner(fich);
             System.out.println("Tabla puntuaciones: ");
             while (sc.hasNext()) {
-                System.out.println(sc.nextLine());
+                System.out.println(sc.nextLine());//leemos cada linea del fichero y la imprimimos
             }
         } catch (FileNotFoundException ex) {
             System.out.println("error2" + ex.getMessage());
         } finally {
-            sc.close();
+            sc.close();//cerramos el canal
         }
     }
 }
